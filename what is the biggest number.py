df96b77761485b3af74d169a4e9b79d369e92c0a
Oscar Stanley <40785@longroad.ac.uk>
2014-10-06 10:24:20 +0100

    done

---
+++ what is the biggest number.py
@@ -0,0 +1,14 @@
+#oscar stanley
+#06-10-14
+# what is the bigger number
+number1 = int(input("What is your first number: "))
+number2 = int(input("What is your second number: "))
+
+if number1 == number2:
+              print("Your numbers you have enterd are the same.")
+
+elif number1 > number2:
+              print("The first number you entered is bigger than the second.")
+
+else:
+              print("The second number you eneterd was bigger.")
