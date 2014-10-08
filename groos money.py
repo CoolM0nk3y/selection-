f126df1cc714d77ba783d86e07db01d8a4b122ea
Oscar Stanley <40785@longroad.ac.uk>
2014-10-08 14:40:44 +0100

    DONE

--- groos money.py
+++ groos money.py
@@ -3,7 +3,12 @@
 # gross pay
 pay = int(input("How much do you get payed per hour? £ "))
 hours = int(input("How many hours do you worked this week: "))
+if hours >= 40:
+    gross = pay * hours
+    gross1 = gross * 1.5
+    print("The amout of money you have made this week is £{0}".format(gross1))
 
-gross = pay * hours
-print("The amout of money you have made this week is £{0}".format(gross))
+else:
+    gross = pay * hours
+    print("The amout of money you have made this week is £{0}".format(gross))
 
