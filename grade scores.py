688681a707807e15674d5bf93bc1d65c5e57a81b
Oscar Stanley <40785@longroad.ac.uk>
2014-10-08 15:08:07 +0100

    done

---
+++ grade scores.py
@@ -0,0 +1,24 @@
+#oscar stanley
+#8-10-14
+# grades
+
+score = int(input("Please enter you score: "))
+
+if score >= 0 and score <=40:
+    print("You got grade U well done")
+
+elif score >= 41 and score <=50:
+    print("You got grade E well done")
+
+elif score >= 51 and score <=60:
+    print("You got grade D well done")
+
+elif score >= 61 and score <=70:
+    print("You got grade C well done")
+
+elif score >= 71 and score <=80:
+    print("You got grade B well done")
+
+elif score >= 81 and score <=100:
+    print("You got grade A well done")
+
